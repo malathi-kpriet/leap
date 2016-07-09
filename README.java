6bfce332ca09e40d705d5e8120016ac9912636fb
malathi-kpriet <malathikanakaraj14@gmail.com>
2016-07-09 14:55:51 +0530

    finish

---
+++ README.java
@@ -0,0 +1,17 @@
+# leap
+class java
+{
+public static void main(string args[])
+{
+int year=2001;
+if(year%4==0)
+{
+System.out.println("leap');
+}
+else
+{
+System.out.println("not a leap year");
+}
+}
+}
+}
